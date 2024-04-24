6fda05049d714c1bebd90494401b9cfa33b8485e
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-04-24 19:16:09 -0300

    feat: creacion del componente Header y actualizacion de estilos SCSS

--- src/pages/home/layout/LayoutHome.jsx
+++ src/pages/home/layout/LayoutHome.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import css from './layout.module.scss';
+import Header from '../header/Header';
 
 export default function LayoutHome() {
-  return <div>LayoutHome</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
