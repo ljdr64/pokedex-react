e00d05c1303c2f954a98a8add95e88825372385e
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-04-25 09:59:16 -0300

    feat: integracion de datos de api rest en componente LayoutHome

---
+++ src/api/apiRest.jsx
@@ -0,0 +1,4 @@
+export const URL_POKEMON =
+  ' https://pokemon-api-opal.vercel.app/api/v1/pokemon/';
+export const URL_ESPECIES =
+  ' https://pokemon-api-opal.vercel.app/api/v1/pokemon-species/';
--- src/pages/home/layout/LayoutHome.jsx
+++ src/pages/home/layout/LayoutHome.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import css from './layout.module.scss';
 import Header from '../header/Header';
+import axios from 'axios';
+import { URL_POKEMON } from '../../../api/apiRest';
 
 export default function LayoutHome() {
+  const [arrayPokemon, setArrayPokemon] = useState({});
+
+  console.log(arrayPokemon);
+
+  useEffect(() => {
+    const api = async () => {
+      const apiPoke = await axios.get(`${URL_POKEMON}`);
+      console.log(apiPoke.data);
+    };
+
+    api();
+  }, []);
+
   return (
     <div>
       <Header />
