ab25b0ac22e768d56c17733c792c3a553c398704
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-04-25 15:49:51 -0300

    feat: creacion de barras de stats en el componente Card y actualizacion de estilos

--- src/pages/home/card/Card.jsx
+++ src/pages/home/card/Card.jsx
@@ -35,6 +35,7 @@ export default function Card({ card }) {
     dataEspecie();
   }, [itemPokemon.id]);
 
+  if (itemPokemon) console.log(itemPokemon);
   const imageSrc = itemPokemon.sprites?.official_artwork;
 
   return (
@@ -46,6 +47,17 @@ export default function Card({ card }) {
         <h4 className={css.altura_poke}> 10cm </h4>
         <h4 className={css.peso_poke}> peso </h4>
         <h4 className={css.habitat_poke}> habitat </h4>
+
+        <div className={css.div_stats}>
+          {itemPokemon.stats &&
+            Object.entries(itemPokemon.stats).map(([stat, value], index) => (
+              <h6 key={index} className={css.item_stats}>
+                <span className={css.name}>{stat}</span>
+                <progress value={value} max={110}></progress>
+                <span className={css.numero}>{value}</span>
+              </h6>
+            ))}
+        </div>
       </div>
     </div>
   );
