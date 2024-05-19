7ce0f8b0c16c2539ab0b24012f2b262d19e95b55
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-05-19 01:52:47 -0300

    feat: diseno del menu de imagenes agregado al header

--- src/pages/home/header/Header.jsx
+++ src/pages/home/header/Header.jsx
@@ -1,10 +1,15 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
 import css from './header.module.scss';
 import logo from '../../../assets/pokemon.png';
 
 export default function Header({ obtenerSearch }) {
   const inputRef = useRef(null);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   const handleClick = () => {
     if (inputRef.current) {
@@ -14,20 +19,52 @@ export default function Header({ obtenerSearch }) {
   };
 
   return (
-    <nav className={css.header}>
-      <div className={css.div_header}>
-        <div className={css.div_logo}>
-          <a href="/">
-            <img src={logo} alt="logo" />
-          </a>
-        </div>
-        <div className={css.div_search}>
-          <div onClick={handleClick}>
-            <FaIcons.FaSearch />
+    <header>
+      <nav className={css.header}>
+        <div className={css.div_header}>
+          <div className={css.div_header_left}>
+            <div className={css.div_logo}>
+              <a href="/">
+                <img src={logo} alt="logo" />
+              </a>
+            </div>
+            <div className={css.div_menu}>
+              <div className={css.menu}>
+                <button onClick={toggleMenu}>Imagenes</button>
+                {menuOpen && (
+                  <ul className={css.menuItems}>
+                    <li>official-artwork</li>
+                    <li>home</li>
+                    <li>dream-world</li>
+                  </ul>
+                )}
+              </div>
+            </div>
           </div>
-          <input ref={inputRef} type="search" placeholder="Buscar" />
+          <div className={css.div_search}>
+            <div onClick={handleClick}>
+              <FaIcons.FaSearch />
+            </div>
+            <input ref={inputRef} type="search" placeholder="Buscar" />
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <nav className={css.header_small}>
+        <div className={css.div_header_small}>
+          <div className={css.div_menu_small}>
+            <div className={css.menu}>
+              <button onClick={toggleMenu}>Imagenes</button>
+              {menuOpen && (
+                <ul className={css.menuItems}>
+                  <li>official-artwork</li>
+                  <li>home</li>
+                  <li>dream-world</li>
+                </ul>
+              )}
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 }
