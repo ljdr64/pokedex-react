681da44f699673514274a9b80b27588abd5e5824
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-11-27 11:37:15 -0300

    feat: mostrar card solo después de cargar la imagen del Pokemon

--- src/pages/home/card/Card.jsx
+++ src/pages/home/card/Card.jsx
@@ -9,10 +9,16 @@ export default function Card({ card, image, onPokemonClick, xpage }) {
   const [especiePokemon, setEspeciePokemon] = useState({});
   const [evolucion, setEvolucion] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [isImageLoading, setIsImageLoading] = useState(true);
   const [xpageChange, setXpageChange] = useState(false);
+
+  const handleImageLoad = () => {
+    setIsImageLoading(false);
+  };
 
   useEffect(() => {
     setXpageChange(true);
+    setIsImageLoading(true);
   }, [xpage]);
 
   const urlImg = (numImg) => {
@@ -83,94 +89,108 @@ export default function Card({ card, image, onPokemonClick, xpage }) {
       ? (itemPokemon.id + 1000).toString().slice(-3)
       : itemPokemon.id;
 
-  if (isLoading || xpageChange) {
-    return <SkeletonCard />;
-  }
-
   return (
-    <div className={css.card}>
-      {itemPokemon.id && (
-        <img
-          className={css.img_poke}
-          src={urlImg(itemPokemon.id)}
-          alt="pokemon"
-        />
-      )}
-      <div className={`bg-${especiePokemon?.color} ${css.sub_card}`}>
-        <strong className={css.id_card}>#{pokeId} </strong>
-        <strong className={css.name_card}> {itemPokemon.name} </strong>
-        <h4 className={css.altura_poke}>Altura: {itemPokemon.height / 10} m</h4>
-        <h4 className={css.peso_poke}>Peso: {itemPokemon.weight / 10} kg</h4>
-        <h4 className={css.habitat_poke}>Habitat: {especiePokemon.habitat}</h4>
+    <>
+      {(isLoading || xpageChange || isImageLoading) && <SkeletonCard />}
+      <div
+        className={css.card}
+        style={{
+          display: isLoading || xpageChange || isImageLoading ? 'none' : 'flex',
+        }}
+      >
+        {itemPokemon.id && (
+          <img
+            className={css.img_poke}
+            src={urlImg(itemPokemon.id)}
+            alt="pokemon"
+            onLoad={handleImageLoad}
+          />
+        )}
+        <div className={`bg-${especiePokemon?.color} ${css.sub_card}`}>
+          <strong className={css.id_card}>#{pokeId} </strong>
+          <strong className={css.name_card}> {itemPokemon.name} </strong>
+          <h4 className={css.altura_poke}>
+            Altura: {itemPokemon.height / 10} m
+          </h4>
+          <h4 className={css.peso_poke}>Peso: {itemPokemon.weight / 10} kg</h4>
+          <h4 className={css.habitat_poke}>
+            Habitat: {especiePokemon.habitat}
+          </h4>
 
-        <div className={css.div_stats}>
-          {itemPokemon.stats &&
-            Object.entries(itemPokemon.stats).map(([stat, value], index) => (
-              <h6 key={index} className={css.item_stats}>
-                <span className={css.name}>{stat}</span>
-                <progress value={value} max={110}></progress>
-                <span className={css.numero}>{value}</span>
-              </h6>
-            ))}
-        </div>
-
-        <div className={css.div_type_color}>
-          {itemPokemon.types &&
-            itemPokemon.types.map((type, index) => {
-              return (
-                <h6 key={index} className={`color-${type} ${css.color_type}`}>
-                  {type}
+          <div className={css.div_stats}>
+            {itemPokemon.stats &&
+              Object.entries(itemPokemon.stats).map(([stat, value], index) => (
+                <h6 key={index} className={css.item_stats}>
+                  <span className={css.name}>{stat}</span>
+                  <progress value={value} max={110}></progress>
+                  <span className={css.numero}>{value}</span>
                 </h6>
-              );
-            })}
-        </div>
+              ))}
+          </div>
 
-        <div className={css.div_evolution}>
-          {evolucion.base_evolution && (
-            <div className={css.item_evo}>
-              <img
-                src={urlImg(evolucion.base_evolution?.id)}
-                alt="evo"
-                className={css.img}
-                onClick={() => {
-                  onPokemonClick(evolucion.base_evolution?.id);
-                }}
-              />
-              <h6 className={css.evo_name}>{evolucion.base_evolution?.name}</h6>
-            </div>
-          )}
-          {evolucion.first_evolution && (
-            <div className={css.item_evo}>
-              <img
-                src={urlImg(evolucion.first_evolution?.id)}
-                alt="evo"
-                className={css.img}
-                onClick={() => {
-                  onPokemonClick(evolucion.first_evolution?.id);
-                }}
-              />
-              <h6 className={css.evo_name}>
-                {evolucion.first_evolution?.name}
-              </h6>
-            </div>
-          )}
-          {evolucion.second_evolution && (
-            <div className={css.item_evo}>
-              <img
-                src={urlImg(evolucion.second_evolution?.id)}
-                alt="evo"
-                className={css.img}
-                onClick={() => {
-                  onPokemonClick(evolucion.second_evolution?.id);
-                }}
-              />
-              <h6 className={css.evo_name}>
-                {evolucion.second_evolution?.name}
-              </h6>
-            </div>
-          )}
+          <div className={css.div_type_color}>
+            {itemPokemon.types &&
+              itemPokemon.types.map((type, index) => {
+                return (
+                  <h6 key={index} className={`color-${type} ${css.color_type}`}>
+                    {type}
+                  </h6>
+                );
+              })}
+          </div>
+
+          <div className={css.div_evolution}>
+            {evolucion.base_evolution && (
+              <div className={css.item_evo}>
+                <img
+                  src={urlImg(evolucion.base_evolution?.id)}
+                  alt="evo"
+                  className={css.img}
+                  onClick={() => {
+                    onPokemonClick(evolucion.base_evolution?.id);
+                  }}
+                  onLoad={handleImageLoad}
+                />
+                <h6 className={css.evo_name}>
+                  {evolucion.base_evolution?.name}
+                </h6>
+              </div>
+            )}
+            {evolucion.first_evolution && (
+              <div className={css.item_evo}>
+                <img
+                  src={urlImg(evolucion.first_evolution?.id)}
+                  alt="evo"
+                  className={css.img}
+                  onClick={() => {
+                    onPokemonClick(evolucion.first_evolution?.id);
+                  }}
+                  onLoad={handleImageLoad}
+                />
+                <h6 className={css.evo_name}>
+                  {evolucion.first_evolution?.name}
+                </h6>
+              </div>
+            )}
+            {evolucion.second_evolution && (
+              <div className={css.item_evo}>
+                <img
+                  src={urlImg(evolucion.second_evolution?.id)}
+                  alt="evo"
+                  className={css.img}
+                  onClick={() => {
+                    onPokemonClick(evolucion.second_evolution?.id);
+                  }}
+                  onLoad={handleImageLoad}
+                />
+                <h6 className={css.evo_name}>
+                  {evolucion.second_evolution?.name}
+                </h6>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
